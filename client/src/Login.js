8c7b4c3d6ffe88c5bf3f418f54b6926a0758dd15
Noah Serr <noah.serr8@gmail.com>
2021-05-12 12:40:14 -0700

    fixed typo in auth url.

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
 
-const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=e10bba1aea88476d8577408b7abffcb2&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email&20user-read-private&20user-top-read%20user-read-playback-state%20user-modify-playback-state"
+const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=e10bba1aea88476d8577408b7abffcb2&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-top-read%20user-read-playback-state%20user-modify-playback-state"
 
 export default function Login() {
     return <Container className="d-flex justify-content-center align-items-center" style={{ minHeight: "100vh" }}>
