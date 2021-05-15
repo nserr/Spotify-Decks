6c00a19da78596535955b4f2527157bbc1937785
Noah Serr <noah.serr8@gmail.com>
2021-05-15 13:29:43 -0700

    added styling to login page.

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,19 +1,32 @@
 import React from 'react'
-import { Container, Button } from 'react-bootstrap'
+import { Container, Button, Row } from 'react-bootstrap'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSpotify } from '@fortawesome/free-brands-svg-icons'
 
-const redirect_uri = "http://localhost:3000"
+import './loginStyles.css'
+
+// const redirect_uri = "http://localhost:3000"
+const redirect_uri = "http://spotifydecks.surge.sh"
+
 const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=e10bba1aea88476d8577408b7abffcb2&response_type=code&redirect_uri=" + redirect_uri + "&scope=user-read-email%20user-read-private%20user-top-read"
+
 
 export default function Login() {
     return (
-        <Container className="d-flex justify-content-center align-items-center">
-            <Button variant="success" size="lg" href={AUTH_URL}>
-                LOGIN WITH <strong>SPOTIFY</strong>
-                <FontAwesomeIcon icon={faSpotify} style={{ marginLeft: "0.5em" }}/>
-            </Button>
+        <Container className="main">
+            <Row>
+                <h1 className="title">Spotify Decks.</h1>
+            </Row>
+            <Row>
+                <p className="subtitle">A unique visualization of your favorite Spotify artists and tracks.</p>
+            </Row>
+            <Row>
+                <Button variant="success" size="lg" href={AUTH_URL}>
+                    LOGIN WITH <strong>SPOTIFY</strong>
+                    <FontAwesomeIcon icon={faSpotify} style={{ marginLeft: "0.5em" }}/>
+                </Button>
+            </Row>
         </Container>
     )
 }
